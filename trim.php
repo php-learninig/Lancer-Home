7fc3e93af633c1917f16fc81e6557fce229e825e
Anna-Eliseeva <eliseeva9797@mail.ru>
2016-10-04 22:25:53 +0300

    шпаргалки пхп

--- trim.php
+++ trim.php
@@ -1,24 +1,4 @@
 <?php
-$ControllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'mops';
-
-echo 'Подключаемый php файл(Контроллер)= ' . $ControllerName . '<br/>';
-
-$actionName = isset($_GET['action']) ? $_GET['action'] : 'test';
-
-echo 'Функция формирующая страницу = ' . $actionName . '<br/>';
-
-
-define('PasthPrefix','../controllers/');
-define('PastPostfix','Controller.php');
-
-function LoadPage($ControllerName, $actionName = 'index'){
-    include_once PasthPrefix . $ControllerName . PastPostfix;
-
-    $function = $actionName . 'Action';
-    $function();
-}
-
-LoadPage($ControllerName,$actionName);
 
 
 
